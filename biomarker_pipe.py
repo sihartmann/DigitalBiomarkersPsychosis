0a52fd87f2b9e50532f2c35ed509cccae98d78f5
oliverW27 <110002879+oliverW27@users.noreply.github.com>
2024-04-02 12:04:23 +1030

    LSA added, interviewer sentence num added

--- biomarker_pipe.py
+++ biomarker_pipe.py
@@ -7,14 +7,15 @@ import opensmile
 import csv
 import shutil
 import multiprocessing
-import whisper
 import nltk
+import regex as re
 from nltk.sentiment import SentimentIntensityAnalyzer
-from nltk.corpus import stopwords
-# from gensim import corpora, models
-# import contractions
+import contractions
+import spacy
 import string
-import re
+from collections import Counter
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
 # Necessary data downloads:
 # nltk.download('maxent_ne_chunker')
@@ -37,6 +38,7 @@ class pipe:
 		print(self.patient_name)
 		self.loglevel = args[2]
 		self.run_mode = args[3]
+		self.audio_interviewer = args[4]
 		stderrhandler = logging.StreamHandler()
 		stderrhandler.setLevel(int(self.loglevel))
 		stderrhandler.setFormatter(logging.Formatter('%(levelname)s: %(message)s'))
@@ -57,8 +59,7 @@ class pipe:
 		self.logger.debug("\tRunning DigBio with command: {}.".format(' '.join(sys.argv)))
 		self.ffmpeg_path = r"C:\Users\a1863615\Downloads\ffmpeg-2024-03-11-git-3d1860ec8d-full_build\bin\ffmpeg.exe"
 		self.whisper_path = r"C:\Users\a1863615\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.12_qbz5n2kfra8p0\LocalCache\local-packages\Python312\Scripts\whisper.exe"
-		feat_detect = r'.\C:\Users\a1863615\Downloads\OpenFace_2.2.0_win_x64\FeatureExtraction.exe'
-
+		self.feat_detect = r"C:\Users\a1863615\Downloads\OpenFace_2.2.0_win_x64\OpenFace_2.2.0_win_x64\FeatureExtraction.exe"
 
 
 
@@ -93,7 +94,8 @@ class pipe:
 		)
 		features = smile.feature_names
 		opensmile_path = f"{self.patient_dir}\\{self.patient_name}"
-		f = open(f"{opensmile_path}_opensmile.csv", 'w')
+		f = open(f"{opensmile_path}_summary_opensmile.csv", 'w')
+		fI = open(f"{opensmile_path}_opensmile.csv", 'w')
 		writer = csv.writer(f)
 		writer.writerow([feature for feature in features])
 		features = smile.process_file(self.audio)
@@ -111,7 +113,7 @@ class pipe:
 		except OSError:
 			self.logger.warning("The log directory already exists. Existing logs will be overwritten.")
 		log_file = self.patient_name + "_whisper.log"
-		self.transcript = f"{self.patient_dir}\\{self.output_name}_whisper.txt"
+		self.transcript = f"{self.patient_dir}\\{self.output_name}.txt"
 		if os.path.isfile(self.transcript):
 			self.logger.info("Transcript for this patient already exists. Skipping transcription...")
 		else:
@@ -120,7 +122,19 @@ class pipe:
 			command = f'{self.whisper_path} -f txt --model {model} audio {linux_audio} --output_dir {self.patient_dir}> {self.log_dir}\\{log_file} 2>&1'
 			self.logger.debug("\tRunning whisper on cleaned patient file using {}.".format(command))
 			subprocess.check_output(command, shell=True)
-			self.logger.info("\twhisper has finished successfully.")
+			self.logger.info("\twhisper (patient) has finished successfully.")
+		
+		log_file = self.patient_name + "_whisper_interviewer.log"
+		self.transcript_int = f"{self.audio_interviewer[:-4]}.txt"
+		if os.path.isfile(self.transcript_int):
+			self.logger.info("Transcript for this interviewer already exists. Skipping transcription...")
+		else:
+			self.linux_audio = f"{self.audio_interviewer}"
+			linux_audio = self.linux_audio.replace('\\','/')
+			command = f'{self.whisper_path} -f txt --model {model} audio {linux_audio} --output_dir {self.patient_dir}> {self.log_dir}\\{log_file} 2>&1'
+			self.logger.debug("\tRunning whisper on cleaned interviewer file using {}.".format(command))
+			subprocess.check_output(command, shell=True)
+			self.logger.info("\twhisper (interviewer) has finished successfully.")
 
 	def run_audio(self):
 		if self.audio.endswith('mp4'):
@@ -135,8 +149,7 @@ class pipe:
 		self.clean_audio(1, 1, -50, -1, 5, -50)
 		self.run_opensmile() 
 		self.run_whisper("base")
-		sys.exit(0)
-		self.run_nltk(f'{self.patient_dir}\\{self.patient_name}_nltk_results.txt')
+		self.run_nltk(f'{self.patient_dir}\\{self.patient_name}_nltk_results.txt', f'{self.patient_dir}\\{self.patient_name}_sim_scores.csv')
 
 	def run_video(self):
 		self.logger.info('Starting openface.')
@@ -150,11 +163,12 @@ class pipe:
 		if not os.path.isfile(self.video):
 			self.logger.error("No video file was found.")
 		command = f'{self.feat_detect} -f {self.video} --out_dir {self.patient_dir} -verbose > {log_path} 2>&1'
+		self.logger.info(f"Running openface with command {command}")
 		exit_c, output = subprocess.getstatusoutput(command)
 		if exit_c != 0:
 			self.logger.error(f'\tOpenface returned exit code {exit_c}. See log file for detailed error message.'.format(exit_c))
 			self.shutdown(1, "\tExecution cancelled due to error in openface.")
-		self.logger.info("\Openface has finished successfully.")
+		self.logger.info("\tOpenface has finished successfully.")
 
 
 	def delete_files(self, files):
@@ -165,9 +179,79 @@ class pipe:
 			except OSError as e:
 				print(f"Error deleting {file}: {e}")
 
+	def preprocess_text(self, text, nlp):     
+		doc = nlp(text.lower())
+		tokens = [token.lemma_ for token in doc]
+		return ' '.join(tokens)
+	
+	def run_nltk(self, output_file, lsa_output):
+		# Open output file
+		file = self.transcript
+		with open(file, 'r') as f:
+			all_text = f.read()
 
-	def run_nltk(self, output_file):
-		pass
+		with open(self.transcript_int, 'r') as f_i:
+			all_int_text = f_i.read()
+		#LSA
+		nlp = spacy.load("en_core_web_sm")
+		sentences = [self.preprocess_text(sent.text,nlp) for sent in nlp(all_text).sents]
+		vectorizer = TfidfVectorizer()
+		X = vectorizer.fit_transform(sentences)
+		similarity_matrix = cosine_similarity(X)
+	
+		with open(lsa_output, "w", newline="") as f:
+			writer = csv.writer(f)
+			head = ["Sentence"] + [sentence for sentence in sentences]
+			writer.writerow(head)
+			for i, sentence in enumerate(sentences):
+				row = [sentence] + [similarity_matrix[i][j] for j in range(len(sentences))]
+				writer.writerow(row)
+		
+		# Fragment into sentences
+		sentences = nltk.tokenize.sent_tokenize(all_text)
+		int_sentences = nltk.tokenize.sent_tokenize(all_int_text)
+		num_sent = len(sentences)
+		num_int_sent = len(int_sentences)
+		# Do sentiment scores per sentence
+		sia = SentimentIntensityAnalyzer()
+		sentiment_scores = [sia.polarity_scores(sentence) for sentence in sentences]
+
+		# Remove punctuation
+		translator = str.maketrans('','', string.punctuation)
+		fixed_text = all_text.translate(translator)
+
+		# Get rid of contractions like I'm, it's, can't ...
+		words = nltk.tokenize.word_tokenize(fixed_text)
+		expanded_text = [contractions.fix(word) for word in words]
+		# print(expanded_text)
+		merged_text = ' '.join(expanded_text)
+		# print(merged_text)
+		# Do POS and dependency tagging.
+		nlp = spacy.load('en_core_web_sm')
+		doc = nlp(merged_text)
+		with open(output_file, 'w') as f:
+			pos_counter = {}
+			dep_counter = {}
+			for token in doc:
+				f.write(f"Token: {token.text}\tPOS: {token.pos_}\tDep: {token.dep_}\n")
+				pos= token.pos_
+				if pos in pos_counter:
+					pos_counter[pos] += 1
+				else:
+					pos_counter[pos] = 1
+				dep = token.dep_
+				if dep in dep_counter:
+					dep_counter[dep] += 1
+				else:
+					dep_counter[dep] = 1
+
+			avg_sentence_length = sum(len(sent.split()) for sent in sentences) / len(sentences)
+			f.write("\nAverage Sentence Length: {}\n".format(avg_sentence_length))
+			f.write("\nSentiment Scores: {}\n".format(sentiment_scores))
+			f.write(f"\nPOS counts: {pos_counter}")
+			f.write(f"\nDependency counter: {dep_counter}")
+			f.write(f"\nNumber of patient sentences: {num_sent}")
+			f.write(f"\nNumber of interviewer sentences: {num_int_sent}")
 
 	def clean_audio(self, start_p, start_d, start_t, stop_p, stop_d, stop_t):
 		self.output_name = self.patient_name + "_cleaned"
@@ -216,16 +300,19 @@ class pipeParser:
 					all_args = []
 					video = None
 					audio = None
+					int_audio = None
 					for root, dirs, files in os.walk(interviews_path):
 						for file in files:
 							print(file, dirs)
 							if (file.startswith('Audio_Participant')):
 								audio = os.path.join(root,file)
+							elif (file.startswith('Audio_Interviewer')):
+								int_audio = os.path.join(root, file)
 							elif file.startswith('Video'):
 								video = os.path.join(root,file)
-						all_args.append([audio, video, log_level, run_mode])
+						all_args.append([audio, video, log_level, run_mode, int_audio])
 					print(all_args[1:])
-					return len(all_args) - 1,all_args[1:]
+					return len(all_args) - 1, all_args[1:]
 				else:
 					print(f"Directory path '{interviews_path}' is not valid.")
 			else:
@@ -255,7 +342,7 @@ def clear_data(path):
 
             for filename in files:
                 file_path = os.path.join(root, filename)
-                if (not ((filename.startswith('Audio') and filename.endswith('mp4')) or filename.startswith('Video')) or (filename.endswith('_cleaned.mp3'))):
+                if (not ((filename.startswith('Audio') and (filename.endswith('mp4') or filename.endswith('mp3'))) or filename.startswith('Video')) or (filename.endswith('_cleaned.mp3'))):
                     print(f"Deleting file: {file_path}")
                     os.remove(file_path)
     except Exception as e:
